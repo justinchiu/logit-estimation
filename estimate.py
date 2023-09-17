a42f702619933d72c3bb14186638b8a903341023
Justin Chiu <justinchiu@g.harvard.edu>
2023-09-16 22:58:51 -0400

    formatting

--- estimate.py
+++ estimate.py
@@ -18,7 +18,8 @@ llama_tokenizer = AutoTokenizer.from_pretrained(llama)
 t5_tokenizer = AutoTokenizer.from_pretrained("t5-small")
 
 gpt_model = AutoModelForCausalLM.from_pretrained(gpt)
-#llama_model = AutoModelForCausalLM.from_pretrained(llama)
+# llama_model = AutoModelForCausalLM.from_pretrained(llama)
+
 
 def get_kl(prefix, K):
     inputs = gpt_tokenizer([prefix], return_tensors="pt")
@@ -32,12 +33,14 @@ def get_kl(prefix, K):
     )
 
     true_dist = Categorical(logits=outputs.scores[0][0])
-    observations = outputs.sequences[:,1]
+    observations = outputs.sequences[:, 1]
 
     V = len(true_dist.enumerate_support())
     counts = torch.zeros(V, dtype=torch.int32)
-    counts = counts.scatter_add(0, observations, torch.ones_like(observations, dtype=torch.int32))
-    sample_estimate = Categorical(probs=counts+1e-5)#counts.log().log_softmax(0)
+    counts = counts.scatter_add(
+        0, observations, torch.ones_like(observations, dtype=torch.int32)
+    )
+    sample_estimate = Categorical(probs=counts + 1e-5)  # counts.log().log_softmax(0)
     sample_kl = kl_divergence(true_dist, sample_estimate)
 
     topk = true_dist.logits.topk(5)
@@ -51,15 +54,13 @@ def get_kl(prefix, K):
     sample_logits = sample_estimate.logits + 0
     sample_logits[topk.indices] = float("-inf")
     sample_mass = sample_logits.logsumexp(0).exp()
-    sample_probs = sample_estimate.probs / sample_mass * (1-topk_mass)
+    sample_probs = sample_estimate.probs / sample_mass * (1 - topk_mass)
     sample_probs[topk.indices] = topk.values.exp()
 
-    sample_topk_kl = kl_divergence(
-        true_dist,
-        Categorical(probs=sample_probs),
-    )
+    sample_topk_kl = kl_divergence(true_dist, Categorical(probs=sample_probs),)
 
     return sample_kl, topk_kl, sample_topk_kl
+
 
 prefix = "Hi"
 kls = []
@@ -80,15 +81,15 @@ for K in Ks:
     kls.append(sample_topk_kl)
     methods.append("sample+top5")
 
-df = pd.DataFrame({'x': Ks_df, 'y': kls, "method": methods})
+df = pd.DataFrame({"x": Ks_df, "y": kls, "method": methods})
 sns.scatterplot(data=df, x="x", y="y", hue="method")
 
-plt.title('Scatter Plot of Num Samples vs KL')
-plt.xlabel('Num samples')
-plt.ylabel('KL')
+plt.title("Scatter Plot of Num Samples vs KL")
+plt.xlabel("Num samples")
+plt.ylabel("KL")
 plt.legend()
 plt.tight_layout()
 plt.savefig("figures/samples_kl.png")
-#import pdb; pdb.set_trace()
+# import pdb; pdb.set_trace()
 
 # use logit bias and greedy?
