9c3ff3411c75204477bbad46dee2b5eea393c964
Justin Chiu <chiu.justin.t@gmail.com>
2023-09-17 01:02:12 -0400

    only gpt3.5 completes incomplete assistant turns

--- compare_asst_repeat.py
+++ compare_asst_repeat.py
@@ -16,10 +16,10 @@ llama_model = AutoModelForCausalLM.from_pretrained(llama)
 
 
 inputs = llama_tokenizer(
-    ["[INST] <<SYS>>\n{system_message}\n<</SYS>>\n\n[INST] Hi"],
+    ["[INST] <<SYS>>\n{system_message}\n<</SYS>>\n\n[INST] Hi [/INST] Hi. How can I"],
     return_tensors="pt",
 )
-outputs = gpt_model.generate(
+outputs = llama_model.generate(
     **inputs,
     max_new_tokens=2,
     return_dict_in_generate=True,
@@ -30,10 +30,10 @@ outputs = gpt_model.generate(
 true_dist = Categorical(logits=outputs.scores[0][0])
 
 inputs = llama_tokenizer(
-    ["[INST] <<SYS>>\n{system_message}\n<</SYS>>\n\n[INST] Hi [/INST]"],
+    ["[INST] <<SYS>>\n{system_message}\n<</SYS>>\n\n[INST] Hi [/INST] Hi. How can I [/INST]"],
     return_tensors="pt",
 )
-outputs2 = gpt_model.generate(
+outputs2 = llama_model.generate(
     **inputs,
     max_new_tokens=2,
     return_dict_in_generate=True,
@@ -46,4 +46,60 @@ dist = Categorical(logits=outputs2.scores[0][0])
 
 sample_kl = kl_divergence(true_dist, dist)
 print(sample_kl)
+
+print(llama_tokenizer.batch_decode(outputs.sequences))
+print(llama_tokenizer.batch_decode(outputs2.sequences))
+
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+response = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Hi."},
+  ],
+  temperature=0,
+  max_tokens=1024
+)
+print(response)
+
+response = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Hi."},
+        {"role": "assistant", "content": "Hi."},
+  ],
+  temperature=0,
+  max_tokens=1024
+)
+print(response)
+
+response = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Hi."},
+        {"role": "assistant", "content": "Hi. How can I help"},
+  ],
+  temperature=0,
+  max_tokens=1024
+)
+print(response)
+
+response = openai.ChatCompletion.create(
+  model="gpt-4",
+  messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Hi."},
+        {"role": "assistant", "content": "Hi. How can I help"},
+  ],
+  temperature=0,
+  max_tokens=1024
+)
+print(response)
 import pdb; pdb.set_trace()
