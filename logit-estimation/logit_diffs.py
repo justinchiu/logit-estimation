db2959ee602cb73644810ddc82e906da816cee7f
Justin Chiu <justinchiu@g.harvard.edu>
2023-09-22 00:44:29 -0400

    try top5 with 3.5-instruct

--- logit-estimation/logit_diffs.py
+++ logit-estimation/logit_diffs.py
@@ -3,8 +3,10 @@ import torch
 import openai
 import tiktoken
 from tenacity import retry, wait_fixed
+from rich.progress import track
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
 
 def binary_search(x, low=-0.5, high=0, eps=1e-3):
     d = torch.zeros_like(x)
@@ -53,21 +55,77 @@ def estimate_topk_logits(logits, K):
     out[idxs] = estimated_logits
     return out
 
-@retry(wait=wait_fixed(1))
-def greedy_complete(message, logit_bias=dict(), model="gpt-3.5-turbo"):
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "Hi."},
-        ],
-        temperature=0,
-        max_tokens=1,
-        logit_bias=logit_bias,
-    )
+
+def openai_api_calculate_cost(usage,model="gpt-3.5-turbo-16k"):
+    pricing = {
+        'gpt-3.5-turbo-4k': {
+            'prompt': 0.0015,
+            'completion': 0.002,
+        },
+        'gpt-3.5-turbo-16k': {
+            'prompt': 0.003,
+            'completion': 0.004,
+        },
+        'gpt-4-8k': {
+            'prompt': 0.03,
+            'completion': 0.06,
+        },
+        'gpt-4-32k': {
+            'prompt': 0.06,
+            'completion': 0.12,
+        },
+        'text-embedding-ada-002-v2': {
+            'prompt': 0.0001,
+            'completion': 0.0001,
+        }
+    }
+
+    try:
+        model_pricing = pricing[model]
+    except KeyError:
+        raise ValueError("Invalid model specified")
+
+    prompt_cost = usage['prompt_tokens'] * model_pricing['prompt'] / 1000
+    completion_cost = usage['completion_tokens'] * model_pricing['completion'] / 1000
+
+    total_cost = prompt_cost + completion_cost
+    print(f"\nTokens used:  {usage['prompt_tokens']:,} prompt + {usage['completion_tokens']:,} completion = {usage['total_tokens']:,} tokens")
+    print(f"Total cost for {model}: ${total_cost:.4f}\n")
+
+    return total_cost
+
+#@retry(wait=wait_fixed(1))
+@retry
+def greedy_complete(
+    message,
+    logit_bias=dict(),
+    model="gpt-3.5-turbo",
+    system=None,
+):
+    system = "You are a helpful assistant." if system is None else system
+    if model == "gpt-3.5-turbo-instruct":
+        response = openai.Completion.create(
+            model=model,
+            prompt=message,
+            temperature=0,
+            max_tokens=1,
+            logit_bias=logit_bias,
+        )
+    else:
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=[
+                {"role": "system", "content": system},
+                {"role": "user", "content": message},
+            ],
+            temperature=0,
+            max_tokens=1,
+            logit_bias=logit_bias,
+        )
     output = response.choices[0].message["content"]
     enc = tiktoken.encoding_for_model(model)
     idx = enc.encode(output)[0]
+    openai_api_calculate_cost(response.usage, model)
     return idx
 
 def binary_search_openai(prefix, logit_bias, model, low=-0.5, high=0, eps=1e-3):
@@ -97,7 +155,7 @@ def binary_search_openai(prefix, logit_bias, model, low=-0.5, high=0, eps=1e-3):
     return mid, idx, num_calls
 
 
-def estimate_topk_logits_openai(prefix, model, K):
+def estimate_topk_logits_openai(prefix, model, K, system=None):
     """
     Estimate the diffs between the elements of the logits vector
     """
@@ -107,8 +165,8 @@ def estimate_topk_logits_openai(prefix, model, K):
     diffs = []
     idxs = []
     total_calls = 0
-    for n in range(K):
-        logit_diff, idx, num_calls = binary_search_openai(prefix, logit_bias, model)
+    for n in track(range(K)):
+        logit_diff, idx, num_calls = binary_search_openai(prefix, logit_bias, model, system=system)
         logit_bias[idx] = -100
         diffs.append(logit_diff)
         idxs.append(idx)
@@ -119,12 +177,23 @@ def estimate_topk_logits_openai(prefix, model, K):
     out[idxs] = estimated_logits
     return out
 
+
 if __name__ == "__main__":
+    model = "gpt-3.5-turbo-instruct"
     model = "gpt-3.5-turbo"
     prefix = "hi"
     idx = greedy_complete(prefix)
 
-    logit_bias = dict()
-    output = binary_search_openai(prefix, logit_bias, model)
-    logits = estimate_topk_logits_openai(prefix, model, 10)
+    #logit_bias = dict()
+    #output = binary_search_openai(prefix, logit_bias, model)
+    logits = estimate_topk_logits_openai(prefix, model, 25)
+
+    response = openai.Completion.create(
+        model=model,
+        prompt=prefix,
+        temperature=0,
+        max_tokens=1,
+        logit_bias=logit_bias,
+        logprobs=5,
+    )
     import pdb; pdb.set_trace()
