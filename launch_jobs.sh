51dc0de8d3b5524f2cc0a51f93708933c0e9b044
Justin Chiu <chiu.justin.t@gmail.com>
2023-09-28 20:17:53 -0400

    launch script

--- launch_jobs.sh
+++ launch_jobs.sh
@@ -2,8 +2,8 @@
 
 for i in {0..100}
 do
-    for k in {1000 2000 4000 8000 16000 32000}
+    for N in {1000 2000 4000 8000 16000 32000}
     do
-        sbatch launch.sub i
+        sbatch launch.sub i N
     done
 done
--- run_estimation.py
+++ run_estimation.py
@@ -4,27 +4,38 @@ from logit_estimation.estimators import naive_estimate, HfSampler, diffsearch
 import random
 from scipy.special import logsumexp
 
-N = 32000
+from pathlib import Path
+
+import argparse
+parser = ArgumentParser()
+parser.add_arguments("index")
+parser.add_arguments("N")
+args = parser.parse_args()
+
+index = args.index
+N = args.N
+#N = 32000
+
+Path(f"saved_logits-{N}").mkdir(exist_ok=True)
 
 dataset = load_dataset("wentingzhao/one-million-instructions")["train"]
-index = random.randint(0, len(dataset))
+#index = random.randint(0, len(dataset))
 d = dataset[index]
 #prefix = f"[INST] <<SYS>>\n{d['system']}\n<</SYS>>\n {d['user']} [/INST]"
 prefix = d["system"] + "\n\n" + d["user"]
-import pdb; pdb.set_trace()
 
 #model = "meta-llama/Llama-2-7b-chat-hf"
 model = "meta-llama/Llama-2-7b-hf"
 sampler = HfSampler(model)
 sampler.sample(prefix, 1)
-np.save(f"saved_logits/{index}-true.npy", sampler.cached_logits.numpy())
+np.save(f"saved_logits-{N}/{index}-true.npy", sampler.cached_logits.numpy())
 
 idxs, estimated_logits, logit_bias, total_calls = diffsearch(sampler, prefix, N)
 lp = np.full((sampler.vocab_size,), float("-inf"), dtype=np.float64)
 Zhat = logsumexp(estimated_logits)
 lp[idxs] = estimated_logits - Zhat
-np.save(f"saved_logits/{index}-diff-{total_calls}.npy", lp)
+np.save(f"saved_logits-{N}/{index}-diff-{total_calls}.npy", lp)
 
 estimator, num_calls = naive_estimate(sampler, prefix, total_calls)
 log_probs = estimator.mean()
-np.save(f"saved_logits/{index}-mc-{total_calls}.npy", log_probs)
+np.save(f"saved_logits-{N}/{index}-mc-{total_calls}.npy", log_probs)
