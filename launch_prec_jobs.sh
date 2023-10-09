2603683fcc45c860f195ece3348651cb0106cdf5
Justin Chiu <chiu.justin.t@gmail.com>
2023-10-09 03:25:18 -0400

    remove 32k

--- launch_prec_jobs.sh
+++ launch_prec_jobs.sh
@@ -2,9 +2,9 @@
 
 for i in {1..100}
 do
-    sbatch launch.sub $i 32000 1e-6
-    sbatch launch.sub $i 32000 1e-4
-    sbatch launch.sub $i 32000 1e-3
-    sbatch launch.sub $i 32000 1e-2
-    sbatch launch.sub $i 32000 1e-1
+    sbatch launch.sub $i "1e-6"
+    sbatch launch.sub $i "1e-4"
+    sbatch launch.sub $i "1e-3"
+    sbatch launch.sub $i "1e-2"
+    sbatch launch.sub $i "1e-1"
 done
