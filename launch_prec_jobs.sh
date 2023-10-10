cde7588a031d7e65cbcf5e1bf6540be61b784680
Justin Chiu <chiu.justin.t@gmail.com>
2023-10-10 03:04:00 -0400

    fix tokenizers dependency

--- launch_prec_jobs.sh
+++ launch_prec_jobs.sh
@@ -2,9 +2,10 @@
 
 for i in {1..100}
 do
-    sbatch launch.sub $i "1e-6"
-    sbatch launch.sub $i "1e-4"
-    sbatch launch.sub $i "1e-3"
-    sbatch launch.sub $i "1e-2"
-    sbatch launch.sub $i "1e-1"
+    #sbatch launch.sub $i "1e-6"
+    sbatch launch.sub $i "1e-5"
+    #sbatch launch.sub $i "1e-4"
+    #sbatch launch.sub $i "1e-3"
+    #sbatch launch.sub $i "1e-2"
+    #sbatch launch.sub $i "1e-1"
 done
--- test.py
+++ test.py
@@ -7,162 +7,9 @@ import numpy as np
 from rich.progress import track
 
 
-precision = "1e-8"
+vector_path = Path("saved_logits-batch")
 
-vector_path = Path("saved_logits-32000")
-files = {
-    "08": [
-        "0-true.npy", 
-        "0-mc-1199045.npy",
-        "0-diff-1199045-eps1e-08.npy",
-    ],
-    "18": [
-        "1-true.npy", 
-        "1-mc-1200501.npy",
-        "1-diff-1200501-eps1e-08.npy",
-    ],
-    "28": [
-        "2-true.npy", 
-        "2-mc-1198224.npy",
-        "2-diff-1198224-eps1e-08.npy",
-    ],
-    "38": [
-        "3-true.npy", 
-        "3-mc-1196708.npy",
-        "3-diff-1196708-eps1e-08.npy",
-    ],
-    "48": [
-        "4-true.npy", 
-        "4-mc-1205012.npy",
-        "4-diff-1205012-eps1e-08.npy",
-    ],
-    "06": [
-        "0-true.npy", 
-        "0-mc-985629.npy",
-        "0-diff-985629-eps1e-06.npy",
-    ],
-    "16": [
-        "1-true.npy", 
-        "1-mc-986990.npy",
-        "1-diff-986990-eps1e-06.npy",
-    ],
-    "26": [
-        "2-true.npy", 
-        "2-mc-985016.npy",
-        "2-diff-985016-eps1e-06.npy",
-    ],
-    "36": [
-        "3-true.npy", 
-        "3-mc-983562.npy",
-        "3-diff-983562-eps1e-06.npy",
-    ],
-    "46": [
-        "4-true.npy", 
-        "4-mc-990877.npy",
-        "4-diff-990877-eps1e-06.npy",
-    ],
-    "04": [
-        "0-true.npy", 
-        "0-mc-774355.npy",
-        "0-diff-774355-eps0.0001.npy",
-    ],
-    "14": [
-        "1-true.npy", 
-        "1-mc-775259.npy",
-        "1-diff-775259-eps0.0001.npy",
-    ],
-    "24": [
-        "2-true.npy", 
-        "2-mc-773567.npy",
-        "2-diff-773567-eps0.0001.npy",
-    ],
-    "34": [
-        "3-true.npy", 
-        "3-mc-771699.npy",
-        "3-diff-771699-eps0.0001.npy",
-    ],
-    "44": [
-        "4-true.npy", 
-        "4-mc-779349.npy",
-        "4-diff-779349-eps0.0001.npy",
-    ],
-    "03": [
-        "0-true.npy", 
-        "0-mc-694407.npy",
-        "0-diff-694407-eps0.001.npy",
-    ],
-    "13": [
-        "1-true.npy", 
-        "1-mc-692164.npy",
-        "1-diff-692164-eps0.001.npy",
-    ],
-    "23": [
-        "2-true.npy", 
-        "2-mc-696107.npy",
-        "2-diff-696107-eps0.001.npy",
-    ],
-    "33": [
-        "3-true.npy", 
-        "3-mc-680163.npy",
-        "3-diff-680163-eps0.001.npy",
-    ],
-    "43": [
-        "4-true.npy", 
-        "4-mc-698277.npy",
-        "4-diff-698277-eps0.001.npy",
-    ],
-    "02": [
-        "0-true.npy", 
-        "0-mc-367187.npy",
-        "0-diff-367187-eps0.01.npy",
-    ],
-    "12": [
-        "1-true.npy", 
-        "1-mc-352955.npy",
-        "1-diff-352955-eps0.01.npy",
-    ],
-    "22": [
-        "2-true.npy", 
-        "2-mc-367356.npy",
-        "2-diff-367356-eps0.01.npy",
-    ],
-    "32": [
-        "3-true.npy", 
-        "3-mc-360216.npy",
-        "3-diff-360216-eps0.01.npy",
-    ],
-    "42": [
-        "4-true.npy", 
-        "4-mc-350889.npy",
-        "4-diff-350889-eps0.01.npy",
-    ],
-    "01": [
-        "0-true.npy", 
-        "0-mc-300980.npy",
-        "0-diff-300980-eps0.1.npy",
-    ],
-    "11": [
-        "1-true.npy", 
-        "1-mc-291352.npy",
-        "1-diff-291352-eps0.1.npy",
-    ],
-    "21": [
-        "2-true.npy", 
-        "2-mc-265957.npy",
-        "2-diff-265957-eps0.1.npy",
-    ],
-    "31": [
-        "3-true.npy", 
-        "3-mc-299686.npy",
-        "3-diff-299686-eps0.1.npy",
-    ],
-    "41": [
-        "4-true.npy", 
-        "4-mc-296890.npy",
-        "4-diff-296890-eps0.1.npy",
-    ],
-
-}
+files = 
 
 # validate files
 for _, fs in files.items():
@@ -174,8 +21,9 @@ example_idxs = set([
     for f in files
     if "diff" in str(f) and precision in str(f)
 ])
-example_idxs = [0,1,2,3,4]
-precisions = [8,6,4,3,2,1]
+example_idxs = list(range(100))
+#precisions = [6,5,4,3,2,1]
+precisions = [6,4,3,2,1]
 
 
 dataset = load_dataset("wentingzhao/one-million-instructions")["train"]
