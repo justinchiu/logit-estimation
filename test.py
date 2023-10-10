3cdfc99b1177184e5e4381d990057e4fb58b1416
Justin Chiu <chiu.justin.t@gmail.com>
2023-10-10 03:44:35 -0400

    1e-5 not running on g2, and have to rewrite test.py

--- test.py
+++ test.py
@@ -9,25 +9,23 @@ from rich.progress import track
 
 vector_path = Path("saved_logits-batch")
 
-files = 
+example_idxs = list(range(100))
+all_files = [f for f in vector_path.iterdir()]
+files = {
+    idx: [f for f in all_files if str(idx) == f.stem.split("-")[0]]
+    for idx in example_idxs
+}
 
 # validate files
 for _, fs in files.items():
     for f in fs:
-        assert (vector_path / f).is_file(), f
-
-example_idxs = set([
-    int(str(f.stem).split("-")[0])
-    for f in files
-    if "diff" in str(f) and precision in str(f)
-])
-example_idxs = list(range(100))
+        assert f.is_file(), f
 #precisions = [6,5,4,3,2,1]
 precisions = [6,4,3,2,1]
 
-
 dataset = load_dataset("wentingzhao/one-million-instructions")["train"]
 
+'''
 experiment, trainer = analyze_utils.load_experiment_and_trainer_from_pretrained(
     "jxm/t5-base__llama-7b__one-million-paired-instructions",
 )
@@ -36,31 +34,61 @@ trainer.model.use_frozen_embeddings_as_input = True
 trainer.args.per_device_eval_batch_size = 1
 
 tokenizer = trainer.tokenizer
+'''
 
 # vocab_size = trainer.embedder_tokenizer.vocab_size
 padded_vocab_size = 32768
 
-for precision in precisions:
+    # batch-diff
+    idxs_batch_diff = []
+    predictions_batch_diff = []
+    references_batch_diff = []
+    calls_batch_diff = []
+    # diff
     idxs_diff = []
     predictions_diff = []
     references_diff = []
+    calls_diff = []
+    # mc
     idxs_mc = []
     predictions_mc = []
     references_mc = []
+    calls_mc = []
+    # true
     idxs = []
     predictions = []
     references = []
+    calls_true = []
 
-    for example_idx in track(example_idxs):
+    #for example_idx in track(example_idxs):
+    for example_idx in example_idxs:
         example = dataset[example_idx]
         prefix = example["system"] + "\n\n" + example["user"]
-        for file in files[f"{example_idx}{precision}"]:
-            f = str(vector_path / file)
+        for file in files[example_idx]:
+            f = str(file)
+            import pdb; pdb.set_trace()
             logprobsnp = np.load(f)
             logprobsnp[logprobsnp < -100] = -30
             logprobs = torch.zeros((1, padded_vocab_size), dtype=torch.float32)
             logprobs[0,:len(logprobsnp)] = torch.tensor(logprobsnp)
-            if "diff" in f:
+            if "batch-diff" in f:
+                output = trainer.generate(
+                    inputs={
+                        "embedder_input_ids": None,
+                        "embedder_attention_mask": None,
+                        "frozen_embeddings": logprobs,
+                    },
+                    generation_kwargs={
+                        "do_sample": False,
+                        "min_new_tokens": 1,
+                        "max_new_tokens": 64,
+                    },
+                )
+                idxs_batch_diff.append(example_idx)
+                predictions_batch_diff.append(tokenizer.batch_decode(output)[0])
+                references_batch_diff.append([prefix])
+                calls_batch_diff.append()
+            elif "diff" in f:
                 output = trainer.generate(
                     inputs={
                         "embedder_input_ids": None,
@@ -76,6 +104,7 @@ for precision in precisions:
                 idxs_diff.append(example_idx)
                 predictions_diff.append(tokenizer.batch_decode(output)[0])
                 references_diff.append([prefix])
+                calls_diff.append()
             elif "mc" in f:
                 output = trainer.generate(
                     inputs={
@@ -92,6 +121,7 @@ for precision in precisions:
                 idxs_mc.append(example_idx)
                 predictions_mc.append(tokenizer.batch_decode(output)[0])
                 references_mc.append([prefix])
+                calls_mc.append()
             elif "true" in f:
                 output = trainer.generate(
                     inputs={
@@ -108,6 +138,7 @@ for precision in precisions:
                 idxs.append(example_idx)
                 predictions.append(tokenizer.batch_decode(output)[0])
                 references.append([prefix])
+                calls_true.append(0)
             else:
                 raise ValueError
 
@@ -117,6 +148,8 @@ for precision in precisions:
     print("GT", bleu_score)
     bleu_score = bleu.compute(predictions=predictions_diff,references=references_diff)
     print("Diff", bleu_score)
+    bleu_score = bleu.compute(predictions=predictions_batch_diff,references=references_batch_diff)
+    print("Batch Diff", bleu_score)
     bleu_score = bleu.compute(predictions=predictions_mc,references=references_mc)
     print("MC", bleu_score)
 
@@ -127,6 +160,10 @@ for precision in precisions:
     output_file = Path(f"outputs/preds_refs_diff_{precision}.txt")
     with output_file.open("w") as f:
         for idx, pred, ref in zip(idxs_diff, predictions_diff, references_diff):
+            f.write(f"{idx}: {ref}\t{pred}\n")
+    output_file = Path(f"outputs/preds_refs_batch_diff_{precision}.txt")
+    with output_file.open("w") as f:
+        for idx, pred, ref in zip(idxs_diff, predictions_batch_diff, references_diff):
             f.write(f"{idx}: {ref}\t{pred}\n")
     output_file = Path("outputs/preds_refs_mc.txt")
     with output_file.open("w") as f:
