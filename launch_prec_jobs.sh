fd8b73e46104a122acd72a5c3500e548fe275944
Justin Chiu <chiu.justin.t@gmail.com>
2023-10-12 14:06:14 -0400

    .

--- launch_prec_jobs.sh
+++ launch_prec_jobs.sh
@@ -3,9 +3,11 @@
 for i in {1..100}
 do
     #sbatch launch.sub $i "1e-6"
-    sbatch launch.sub $i "1e-5"
+    #sbatch launch.sub $i "1e-5"
     #sbatch launch.sub $i "1e-4"
     #sbatch launch.sub $i "1e-3"
     #sbatch launch.sub $i "1e-2"
     #sbatch launch.sub $i "1e-1"
+    sbatch launch.sub $i "5e-1"
+    sbatch launch.sub $i "1"
 done
